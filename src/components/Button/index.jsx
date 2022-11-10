db1722d28196e6fdf3ac141166aec23131455e85
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-10 12:04:45 -0300

    feat: button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+const Button = ({label}) => {
+
+    return (
+        <button className='Button'>{label}</button>
+    )
+}
+
+export default Button
--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './styles.css'
+import Button from '../Button'
 
 const Calculadora = () => {
 
     return (
         <div className='Calculadora'>
-
+            <Button label="AC" />
+            <Button label="/" />
+            <Button label="7" />
+            <Button label="8" />
+            <Button label="9" />
+            <Button label="*" />
+            <Button label="4" />
+            <Button label="5" />
+            <Button label="6" />
+            <Button label="-" />
+            <Button label="1" />
+            <Button label="2" />
+            <Button label="3" />
+            <Button label="+" />
+            <Button label="0" />
+            <Button label="." />
+            <Button label="=" />
         </div>
     )
 }
